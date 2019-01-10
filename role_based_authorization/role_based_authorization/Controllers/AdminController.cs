6d037b7fb919f3a2f41592c366d2f7ee5fe4e1c1
Dario Sartori <dario.sartori@gmx.ch>
2019-01-10 22:19:29 +0100

    add role based authorization

---
+++ role_based_authorization/role_based_authorization/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace role_based_authorization.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Dashboard()
+        {
+            var currentUser = (string)Session["username"];
+            var userRoles = MvcApplication.userRoles;
+            var currentUserRole = (string)userRoles[currentUser];
+
+            if (currentUserRole == "Administrator")
+            {
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
